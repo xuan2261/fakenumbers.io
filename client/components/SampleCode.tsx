d146ad3b79024a1f26da193df9af2073a3f48d14
Phuoc Nguyen <me@phuoc.ng>
2020-02-06 15:43:05 +0700

    Add highlighting code component

---
+++ client/components/SampleCode.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import highlight from '../helpers/highlight';
+
+interface SampleCodeProps {
+    code: string;
+    fullHeight?: boolean;
+    lang?: string;
+}
+
+const SampleCode: React.FC<SampleCodeProps> = ({ code, fullHeight = false, lang = "javascript" }) => {
+    return code === ''
+            ? <></>
+            : (
+                <pre
+                    className="hljs"
+                    style={{
+                        height: fullHeight ? '100%' : 'auto',
+                        lineHeight: 1.5,
+                        margin: 0,
+                    }}
+                    dangerouslySetInnerHTML={{ __html: highlight(code, lang) }}
+                />
+            );
+};
+
+export default SampleCode;
