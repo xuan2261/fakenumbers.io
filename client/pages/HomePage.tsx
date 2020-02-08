54c9d643a6777ac36a6a14e2d3fc08d46bd25668
Phuoc Nguyen <me@phuoc.ng>
2020-02-08 22:12:37 +0700

    Tweak the home page

--- client/pages/HomePage.tsx
+++ client/pages/HomePage.tsx
@@ -18,11 +18,14 @@ const HomePage = () => {
         <Layout>
             <Helmet>
                 <title>Fake Numbers - Generate fake numbers</title>
-                <meta name="description" content="Generate fake numbers" />
+                <meta name='description' content='Generate fake numbers' />
             </Helmet>
-            <h1 className="font-bold mt-48 mb-12 text-5xl text-center">Fake a number. Check a number.</h1>
-            <div className="flex flex-column justify-center">
-                <div className="shadow-2xl">
+            
+            <h1 className='font-bold mt-32 text-5xl text-center'>Fake a number. Check a number.</h1>
+            <h3 className='font-light mb-16 text-3xl text-center'>available as JS libary and online tool</h3>
+
+            <div className='flex justify-center'>
+                <div className='border border-gray-400 shadow-2xl'>
                     <SampleCode code={`
 import { creditCard } from '@phuocng/fake-numbers';
 
@@ -33,12 +36,14 @@ const creditCardNumber = creditCard.fake();
 creditCard.check(creditCardNumber).valid; // true
 `}/>
                 </div>
+            </div>
 
+            <div className='text-center mt-12 mb-32'>
                 <Link
-                    to="/numbers"
-                    className='text-2xl'
+                    to='/numbers'
+                    className='text-white bg-black px-4 py-3 text-2xl'
                 >
-                    Supported numbers
+                    See Supported Numbers
                 </Link>
             </div>
         </Layout>
