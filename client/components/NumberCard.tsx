1681d9239fc97cf993281b5ad7e460367cb1171a
Phuoc Nguyen <me@phuoc.ng>
2020-02-08 19:03:47 +0700

    Add zig-zag border

--- client/components/NumberCard.tsx
+++ client/components/NumberCard.tsx
@@ -11,7 +11,7 @@ interface NumberCardProps {
 const NumberCard: React.FC<NumberCardProps> = ({ numberType }) => {
     const meta = NumberMetaMap.get(numberType);
     return (
-        <div className='w-1/4 px-4 mb-8'>
+        <div className='w-1/4 px-4 mt-8'>
             <div className='rounded border border-gray-400 h-full text-center'>
                 <Link to={`/numbers/${numberType}`} className='flex flex-col h-full'>
                     <div className='p-2 border-b border-gray-400 text-2xl'>
--- client/pages/Footer.tsx
+++ client/pages/Footer.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer: React.FC<{}> = () => {
+    const zigZag = "url(\"data:image/svg+xml;utf-8,<svg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='9px' height='6px' viewBox='0 0 9 6'><polygon opacity='1' points='4.5,4.5 0,0 0,1.208 4.5,5.708 9,1.208 9,0' /></svg>\")";
+
     return (
+        <>
+        <div
+            className='bg-repeat-x w-full h-2 my-12'
+            style={{ backgroundImage: zigZag }}
+        />
         <div className='ml-auto mr-auto w-3/4 my-16'>
             <div className='flex'>
                 <div className='flex-1'>
@@ -113,6 +120,7 @@ const Footer: React.FC<{}> = () => {
                 <div>This site is <a className="border-b border-black" href="https://github.com/phuoc-ng/fakenumbers.io" rel="noopener noreferrer" target="_blank">open source</a></div>
             </div>
         </div>
+        </>
     );
 };
 
