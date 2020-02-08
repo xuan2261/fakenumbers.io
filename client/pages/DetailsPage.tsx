f2c0b04ce21b4745037d6a5aa940e1349118ae3e
Phuoc Nguyen <me@phuoc.ng>
2020-02-08 11:16:57 +0700

    Add Tab component

--- client/pages/DetailsPage.tsx
+++ client/pages/DetailsPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as fakeNumbers from '@phuocng/fake-numbers';
 
 import NumberType from '../constants/NumberType';
@@ -10,14 +10,51 @@ interface DetailsPageProps {
     numberType: NumberType;
 }
 
+interface TabProps {
+    tabIndex: number;
+}
+
 const DetailsPage: React.FC<DetailsPageProps> = ({ numberType }) => {
+    const [activeTab, setActiveTab] = useState(0);
     const { fake, check } = fakeNumbers[numberType];
+
+    const Tab: React.FC<TabProps> = ({ tabIndex, children }) => {
+        const isActive = tabIndex === activeTab;
+        const click = () => setActiveTab(tabIndex);
+        return (
+            <div
+                className={`border-b-4 cursor-pointer px-5 py-3 text-2xl ${isActive ? 'border-red-400 font-bold' : 'border-transparent font-light'}`}
+                onClick={click}
+            >
+                {children}
+            </div>
+        );
+    };
 
     return (
         <div>
             <div>Type: {numberType}</div>
-            <FakeTab numberType={numberType} fake={fake} />
-            <CheckTab numberType={numberType} check={check} />
+
+            <div>
+                <div
+                    style={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Tab tabIndex={0}>
+                        Fake
+                    </Tab>
+                    <Tab tabIndex={1}>
+                        Check
+                    </Tab>
+                </div>
+                <div className='border border-gray-400'>
+                    { activeTab === 0 && <FakeTab numberType={numberType} fake={fake} /> }
+                    { activeTab === 1 && <CheckTab numberType={numberType} check={check} /> }
+                </div>
+            </div>
         </div>
     );
 };
